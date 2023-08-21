dee8d27d9cc7ff4275ffe379ccb4d3549f4e5d38
maryfiona <aguchidimmamaryfiona@gmail.com>
2023-08-21 07:58:38 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './Component/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const Navbar =()=>{
+    return(
+        <>
+        <h1 className="px-14 p-14 font-sans">hello world</h1>
+
+        </>
+    )
+}
+export default Navbar;
+
